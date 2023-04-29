b9a4bb13066fd73aad55c2f07dd1dac108d10699
kuromitsu0104 <kuromitsu0104@gmail.com>
2023-04-29 18:07:17 +0900

    [change] tailwindの導入

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 module.exports = {
+  darkMode: 'light',
   content: [
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
-  ]
-}
+    './app/javascript/**/*.js',
+    'preline/dist/*.js',
+  ],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/container-queries'),
+  ],
+};
